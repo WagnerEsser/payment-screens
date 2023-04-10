635338f5f262b7f9793a789f9f77c8f5bc38968c
Wagner Esser <waesser@gmail.com>
2023-04-10 10:22:03 -0300

    add nunito font

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { Box } from '@mui/material'
+import { Box, ThemeProvider, createTheme } from '@mui/material'
 import PixScreen from './screens/PixScreen'
 import GlobalStyles from './styles/global'
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Nunito'
+  }
+})
+
 const App = (): JSX.Element => (
-  <Box display="flex" flexDirection="column" width="100%" height="100%" position="relative">
-    <GlobalStyles />
-    <PixScreen />
-  </Box>
+  <ThemeProvider theme={theme}>
+    <Box display="flex" flexDirection="column" width="100%" height="100%" position="relative">
+      <GlobalStyles />
+      <PixScreen />
+    </Box>
+  </ThemeProvider>
 )
 
 export default App
--- src/screens/PixScreen/index.tsx
+++ src/screens/PixScreen/index.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material'
-import Header from '../../components/Header'
+import Header from 'src/components/Header'
 
 const PixScreen = (): JSX.Element => (
   <Box>
