107e0cb245b2137d9c75a1c68003626db09fb084
Wagner Esser <waesser@gmail.com>
2023-04-11 17:47:21 -0300

    fix(config): prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,49 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "eslint:recommended",
-    "standard-with-typescript",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "prettier",
-    "prettier/prettier",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+    'prettier/prettier',
+    'plugin:prettier/recommended'
   ],
   overrides: [
     {
-      files: ["*.ts", "*.tsx", "*.d.ts"],
+      files: ['*.ts', '*.tsx', '*.d.ts'],
       parserOptions: {
-        project: ["./tsconfig.json"],
-      },
-    },
+        project: ['./tsconfig.json']
+      }
+    }
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
-  plugins: ["react", "@typescript-eslint", "prettier", "react-hooks"],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
   rules: {
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "prettier/prettier": "error",
-    "@typescript-eslint/triple-slash-reference": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/restrict-plus-operands": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
   settings: {
     react: {
-      version: "detect",
-    },
-  },
-};
+      version: 'detect'
+    }
+  }
+}
